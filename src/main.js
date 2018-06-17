3704a89b3e9ad98ee9f2ad81a41de9daf733ed5e
natesevn <nikoniko562@gmail.com>
2018-06-16 17:58:20 -0700

    barebones spa

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-import React, { Component } from 'react';
-import Butter from 'buttercms'
+import Butter from 'buttercms';
+import React, { Component } from "react";
+import {
+	Route,
+	NavLink,
+	HashRouter
+} from "react-router-dom";
+import Home from "./home";
+import Stuff from "./stuff";
+import Contact from "./contact";
 
 const butter = Butter('302d2120f0ad6f28bdf6ec37ba9e4a4a577db0f6');
 
@@ -27,17 +35,22 @@ class Main extends Component {
 			const homepage = this.state.content;
 
 			return (
+				<HashRouter>
 				<div>
-          			<h1>Simple SPA</h1>
-          			<ul className="header">
-            			<li><a href="/">Home</a></li>
-            			<li><a href="/stuff">Stuff</a></li>
-            			<li><a href="/contact">Contact</a></li>
-          			</ul>
-          			<div className="content">
-          			</div>
-        		</div>
-            );
+				<h1>Simple SPA</h1>
+				<ul className="header">
+				<li><NavLink to="/">Home</NavLink></li>
+				<li><NavLink to="/stuff">Stuff</NavLink></li>
+				<li><NavLink to="/contact">Contact</NavLink></li>
+				</ul>
+				<div className="content">
+  <Route exact path="/" component={Home}/>
+  <Route path="/stuff" component={Stuff}/>
+  <Route path="/contact" component={Contact}/>
+</div>
+				</div>
+				</HashRouter>
+				);
 		} else {
 			return (
 				<div>
